25206835034bff709f454a1ac3663e16cf229f16
szagi3891 <szeligagrzegorz@gmail.com>
2016-05-27 05:43:19 +0200

    dodanie menu

--- src/Index.elm
+++ src/Index.elm
@@ -8,9 +8,10 @@ import List
 import Http
 import Task
 
+init_model = { path = ["cos", "dasda"] }
 
 main = Html.App.program {
-    init = ({}, Cmd.none),
+    init = (init_model, Cmd.none),
     view = view,
     update = update,
     subscriptions = subscriptions
@@ -22,26 +23,35 @@ subscriptions model = Sub.none
 
 view model =
     div [class "container"] [
-        div [class "panel_path"] [
-            div [class "panel_path_item"] [text "."]
-        ]
+
+        div [class "panel_path"]
+            (List.map makePathItem (["."] ++ model.path))
+
         , div [class "menu"] []
         , div [class "panel_content"] [
-            div [class "panel_left"] []
+            div [class "panel_left"] makeLeftList
             , div [class "panel_right"] [
                 button [] [text "Edit"]
             ]
         ]
     ]
 
-type Msg = Show
+makePathItem name = span [class "panel_path_item"] [text name]
+
+makeLeftList = List.map makeLeftListItem ["..", "cosik", "cosik2", "cosik4334"]
+makeLeftListItem name = div [class "left_item", onClick (LeftClick name)] [text name]
+
+
+
+type Msg = LeftClick String
 
 
 -- update : Msg Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Show ->
-            (model, Cmd.none)
+                                                        -- lewe menu kliknięte
+        LeftClick name ->
+            ({model | path = (model.path ++ [name])}, Cmd.none)
 
 
 -- Task.mapError : (x -> y) -> Task x a -> Task y a
