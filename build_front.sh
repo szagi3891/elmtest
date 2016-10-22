a5d9fb0cb579d0f231f0f25925d76aac02ce3bea
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-22 21:55:48 +0200

    fix script build

--- build_front.sh
+++ build_front.sh
@@ -1,2 +1,4 @@
-elm-make ./src/front/src/Test.elm --output ./src/front/build/test.elm.js
-elm-make ./src/front/src/Index.elm --output ./src/front/build/index.elm.js
+cd src/front
+
+elm-make ./src/Test.elm --output ./build/test.elm.js
+elm-make ./src/Index.elm --output ./build/index.elm.js
