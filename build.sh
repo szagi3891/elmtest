aae91399b6b8a61d3dd4e806fdd2d383c7106d71
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-22 21:45:42 +0200

    refactor

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-elm-make ./src/Test.elm --output ./static/build/test.elm.js
-elm-make ./src/Index.elm --output ./static/build/index.elm.js
+./build_front.sh
+./build_server.sh
---
+++ build_front.sh
@@ -0,0 +1,2 @@
+elm-make ./src/front/src/Test.elm --output ./src/front/build/test.elm.js
+elm-make ./src/front/src/Index.elm --output ./src/front/build/index.elm.js
