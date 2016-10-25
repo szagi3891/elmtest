9369c421b4190b76eea83cd03599696d828eba66
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-25 05:02:21 +0200

    add favicon.ico

--- src/server/src/routing.rs
+++ src/server/src/routing.rs
@@ -31,6 +31,8 @@ pub fn process_router<'a>(
         return;
     }
 
+    //http://www.freefavicon.com/freefavicons/objects/iconinfo/a-new-computer-152-4714.html
+    
     for (prefix, prefix_path) in (*static_path).iter() {
 
         if router.eq(prefix.as_str()) {
@@ -46,16 +48,34 @@ pub fn process_router<'a>(
                 }
 
                 let file_to_open = format!(".{}{}", prefix_path, file_path);
-
+                
+                                                                //TODO - remove this line
                 println!("open {:?}", file_to_open);
-                                                                                        //TODO - error handling
-                let mut file = File::open(file_to_open).unwrap();
                 
-                let mut s = String::new();
-                file.read_to_string(&mut s).unwrap();
-                
-                out_response.send(s.as_bytes());
-                return;
+                match File::open(file_to_open) {
+
+                    Ok(mut file) => {
+
+                        let mut file_data: Vec<u8> = Vec::new();
+
+                        match file.read_to_end(&mut file_data) {
+                            Ok(_) => {
+                                
+                                out_response.send(file_data.as_slice());
+                                return;
+                            }
+                            Err(err) => {
+                                out_response.send("error read".as_bytes());
+                                return;
+                            },
+                        }
+                    },
+
+                    Err(err) => {
+                        out_response.send("error open".as_bytes());
+                        return;
+                    },
+                };
             }
             
             out_response.send(format!("dopasowano - brak urla {:?} {:?} {:?}", prefix, prefix_path, url).as_bytes());
