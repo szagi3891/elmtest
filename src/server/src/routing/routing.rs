d55df4f0c9883c84a9179afabe945a97b072167e
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-26 22:21:13 +0200

    static serve

--- src/server/src/routing/routing.rs
+++ src/server/src/routing/routing.rs
@@ -1,12 +1,9 @@
 use lib::router::Router;
 use lib::outresponse::OutResponse;
 use std::collections::HashMap;
-use std::fs::File;
 
-use lib::get_file_type::get_file_type;
 use lib::response_type::ResponseType;
-
-use std::io::Read;
+use routing::serve_static;
 
                                         //TODO - move to mod.rs
 
@@ -30,58 +27,9 @@ pub fn process_router<'a>(
     for (prefix, prefix_path) in (*static_path).iter() {
 
         if router.eq(prefix.as_str()) {
-            
             let url = router.url();
 
-            if let Some(ref file_path) = url {
-                
-                if let Some(_) = file_path.find("..") {
-                    panic!("niedozwolona fraza");
-                            //TODO - dorobić poprawną obsługę
-                            //sprawdzić czy w to odgałęzienie wchodzi program prawidłowo
-                            //normalizować ścieżkę i sprawdzać czy początek zgadza się ze ścieżką bazową
-                }
-
-                let file_to_open = format!(".{}{}", prefix_path, file_path);
-                
-                                                                //TODO - remove this line
-                println!("open {:?}", file_to_open);
-
-                let response_type_opt = get_file_type(file_to_open.as_str());
-
-                match File::open(file_to_open) {
-
-                    Ok(mut file) => {
-
-                        let mut file_data: Vec<u8> = Vec::new();
-
-                        match file.read_to_end(&mut file_data) {
-                            Ok(_) => {
-                                
-                                match response_type_opt {
-                                    Some(response_type) => {
-                                        out_response.send(response_type, file_data.as_slice());
-                                    }
-                                    None => {
-                                        out_response.send(ResponseType::ServerError, file_data.as_slice());
-                                    }
-                                }
-                            }
-                            Err(err) => {
-                                out_response.send(ResponseType::Html, "error read".as_bytes());
-                            },
-                        }
-                    },
-
-                    Err(err) => {
-                        out_response.send(ResponseType::Html, "error open".as_bytes());                        
-                    },
-                };
-
-                return;
-            }
-            
-            out_response.send(ResponseType::Html, format!("dopasowano - brak urla {:?} {:?} {:?}", prefix, prefix_path, url).as_bytes());
+            serve_static::serve(out_response, prefix_path, url);
 
             return;
         }
---
+++ src/server/src/routing/serve_static.rs
@@ -0,0 +1,60 @@
+use lib::get_file_type::get_file_type;
+use lib::outresponse::OutResponse;
+use lib::response_type::ResponseType;
+
+use std::fs::File;
+use std::io::Read;
+
+pub fn serve(out_response: OutResponse, prefix_path: &String, url: Option<String>) {    
+
+    if let Some(ref file_path) = url {
+
+        if let Some(_) = file_path.find("..") {
+            panic!("niedozwolona fraza");
+                    //TODO - dorobić poprawną obsługę
+                    //sprawdzić czy w to odgałęzienie wchodzi program prawidłowo
+                    //normalizować ścieżkę i sprawdzać czy początek zgadza się ze ścieżką bazową
+        }
+
+        let file_to_open = format!(".{}{}", prefix_path, file_path);
+
+                                                        //TODO - remove this line
+        println!("open {:?}", file_to_open);
+
+        let response_type_opt = get_file_type(file_to_open.as_str());
+
+        match File::open(file_to_open) {
+
+            Ok(mut file) => {
+
+                let mut file_data: Vec<u8> = Vec::new();
+
+                match file.read_to_end(&mut file_data) {
+                    Ok(_) => {
+
+                        match response_type_opt {
+                            Some(response_type) => {
+                                out_response.send(response_type, file_data.as_slice());
+                            }
+                            None => {
+                                out_response.send(ResponseType::ServerError, file_data.as_slice());
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        out_response.send(ResponseType::Html, "error read".as_bytes());
+                    },
+                }
+            },
+
+            Err(err) => {
+                out_response.send(ResponseType::Html, "error open".as_bytes());                        
+            },
+        };
+
+        return;
+    }
+
+    out_response.send(ResponseType::Html, format!("dopasowano - brak urla {:?} {:?}", prefix_path, url).as_bytes());
+
+}
