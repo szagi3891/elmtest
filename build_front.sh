675e84ab680f42ed30a060ccb9ec0b46fa79142a
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-28 03:24:49 +0200

    refactor structure front

--- build_front.sh
+++ build_front.sh
@@ -1,4 +1,4 @@
 cd src/front
 
-elm-make ./src/Test.elm --output ./build/test.elm.js
-elm-make ./src/Index.elm --output ./build/index.elm.js
+elm-make ./src/test/Test.elm --output ./build/test.elm.js
+elm-make ./src/proto/Index.elm --output ./build/proto.elm.js
