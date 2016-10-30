64c405ceb14cc80417be8c9db2f9f39968b2680a
szagi3891 <szeligagrzegorz@gmail.com>
2016-10-30 06:02:12 +0100

    BlobStore - make main set loop

--- src/server/src/lib/blob_stor.rs
+++ src/server/src/lib/blob_stor.rs
@@ -16,6 +16,12 @@ enum DirMode {
     ContentDir,
 }
 
+enum DirSetCommand {
+    NeedInit,
+    ReedSuccess,
+    NeedSubDir(u8),
+}
+
 impl<'a> BlobStor<'a> {
 
     pub fn new(base_path: &'a str) -> BlobStor<'a> {
@@ -42,9 +48,63 @@ impl<'a> BlobStor<'a> {
 impl Dir {
 
     fn get(&mut self, hash: &[u8]) -> String {
+        //TODO
         return "dasdas".to_string();
     }
     
     fn set(&mut self, hash: &[u8], content: &[u8]) {
+        
+        let mut count_loop = 0;
+        
+        loop {
+            count_loop += 1;
+            if (count_loop > 10) {
+                panic!("too much recursion");
+            }
+            
+            match (self.set_exec(hash, content)) {
+                DirSetCommand::NeedInit => {
+                        
+                    //TODO - odczytanie początkowej struktury katalogu
+                    unimplemented!();
+                },
+
+                DirSetCommand::ReedSuccess => {
+                    return;
+                },
+
+                DirSetCommand::NeedSubDir(_) => {
+
+                    //TODO - utworzenie kolejnego podkatalogu
+                    unimplemented!();
+                },
+            }
+        }
+    }
+
+    fn set_exec(&mut self, hash: &[u8], content: &[u8]) -> DirSetCommand {
+        
+        let guard = self.inner.read().unwrap();
+
+        match *guard {
+            DirMode::Uninitialized => DirSetCommand::NeedInit,
+            DirMode::ContentFiles => {
+                //jeśli licznik jest ok,
+                    //TODO - odpal procedurę czytania
+                    DirSetCommand::ReedSuccess
+                //w przeciwnym razie, zwróć informację ze statusem, że ten katalog wymaga przebudowania na ContentDir
+            },
+            DirMode::ContentDir => {
+                //weź podkatalog
+                //istnieje
+                    //odpal metodę set na tym podkatalogu
+                //nie istnieje
+                    DirSetCommand::NeedSubDir(0x43)
+            },
+        }
+    }
+
+    fn initialize(&mut self) {
+        //czytanie struktury tego katalogu
     }
 }
